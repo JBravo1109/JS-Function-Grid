3b16d0d7aa9481c9d934eed6e44727e535929ebe
Joel Bravo <jbravo@aclanyc.com>
2017-02-27 13:21:31 +0000

    pixel art

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //Write your function calls below this line
+setColor(0 ,0 , "green");
+setColor(0, 9, "green");
+setColor(9, 0, "green" );
+setColor(9, 9, "green");
+
+setColor(6, 1,"black");
+setColor(7,2,"black");
+setColor(8,3,"black");
+setColor(8,4, "black");
+setColor(8, 5, "black");
+setColor(8,6,"black");
+setColor(7, 7,"black");
+setColor(6,8, "black");
+setColor(3,3, "black");
+setColor(3, 6, "black");
